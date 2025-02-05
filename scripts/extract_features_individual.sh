fee1756eb1b83e9d91b8b41525aabef1caffa961
Daniel-Lin-S <75996224+Daniel-Lin-S@users.noreply.github.com>
2025-02-05 08:27:18 +0800

    separated two feature extractions

--- scripts/extract_features_individual.sh
+++ scripts/extract_features_individual.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
-SAMPLE_NAME="width[5.0]-overlap[2.5]-preictal[300.0]-nrand[50]"
-SAMPLE_PATH="./samples/clustering/$SAMPLE_NAME.h5"
+PATIENT_ID="DA001023"
+SAMPLE_NAME="width[5.0]-overlap[2.5]-preictal[3600.0]-nrand[1000]"
+SAMPLE_PATH="./samples/clustering/$PATIENT_ID/$SAMPLE_NAME.h5"
 TF_METHOD='cwt'
-FEATURE_REPO="./samples/timefreq"
+FEATURE_REPO="./samples/timefreq/$PATIENT_ID"
 FEATURE_PATH="$FEATURE_REPO/$SAMPLE_NAME-timefreq[$TF_METHOD].h5"
 
 [ ! -d "$FEATURE_REPO" ] && mkdir "$FEATURE_REPO"
